5eedb3f2d93fb2209f9e0dbcc74b6017bd27035b
Nasihatiko <knasyikat@gmail.com>
2022-05-07 19:37:06 +0600

    validation

--- video/forms.py
+++ video/forms.py
@@ -7,14 +7,15 @@ class CreateVideoForm(ModelForm):
         model = Video
         exclude = ('user', 'created_at', 'updated_at', 'slug')
 
-
     def clean(self):
         slug = self.cleaned_data.get('title').lower().replace(" ", '-')
         if Video.objects.filter(slug=slug).exists():
             raise ValidationError('Slug with such name already exists!')
         return self.cleaned_data
 
+
 class UpdateVideoForm(ModelForm):
     class Meta:
         model = Video
         exclude = ('user', 'created_at', 'updated_at', 'slug')
+
