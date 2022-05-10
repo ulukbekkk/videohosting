06123ca99ed5b9a9f2614e335393dd423845c379
Uluk <nelson.mandela.99.99@gmail.com>
2022-05-10 15:22:38 +0600

    templates and some funcion fixed

--- parsing/parsings.py
+++ parsing/parsings.py
@@ -35,18 +35,7 @@ body = get_all_news_body()
 img = get_all_news_images()
 
 zipped = list(zip(title, body, img))
-print(zipped)
 
-dict_parsing = {}
-count = 1
-# for a in range(0, len(title)):
-#     dict_parsing[count] = list(title[a], body[a], img
-#
-#
-# for m in dict_parsing.values():
-#     print(m)
 
-# a, b, c = dict_parsing.values()
-#
-# print( a,b,c)
+
 
