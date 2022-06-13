196a6969b0f2bda89a35f25b6aaa2ffa4737f4e2
Uluk <nelson.mandela.99.99@gmail.com>
2022-06-13 13:44:52 +0600

    fixxed

--- config/settings.py
+++ config/settings.py
@@ -163,7 +163,7 @@ EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_USE_TLS = True
 EMAIL_PORT = 587
 EMAIL_HOST_USER = config('EMAIL_USER')
-EMAIL_HOST_PASSWORD = 'vdvuqucmdmyikffz'
+EMAIL_HOST_PASSWORD = config('EMAIL_PASSWORD')
 
 
 
