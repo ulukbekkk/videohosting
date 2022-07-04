a3226124eea5287acce1e9d869974925d9f598ee
Uluk <nelson.mandela.99.99@gmail.com>
2022-07-04 17:11:01 +0600

    domain fixxed

--- myuser/heplers.py
+++ myuser/heplers.py
@@ -1,11 +1,13 @@
 from django.core.mail import send_mail
+from decouple import config
 
+DOMEIN = config("DOMEIN")
 
 def send_activation_mail(user):
     message = f"""
 Thank for registration.
 Activate your account by sent email:
-http://127.0.0.1:8000/account/activate/{user.activation_code}
+{DOMEIN}/account/activate/{user.activation_code}
 """
     send_mail(
         'Активация аккаунта',
